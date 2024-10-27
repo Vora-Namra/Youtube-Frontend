fdf6c122cea5141a15c7bcec0f1f89dd6bd063a1
Namra Vora <voranamra625@gmail.com>
2024-10-27 14:50:05 +0530

    updating image & logo

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -4,9 +4,10 @@ import { SearchBar } from './SearchBar'
 function AppBar() {
   return (
     <div className='flex justify-between mt-4 mb-2'>
-        <div className='text-md inline-flex items-center '>Youtube</div>
+        <div className='inline-flex items-center pl-6'><img src="/image.png" width='50' alt="" /></div>
+        
         <div><SearchBar/></div>
-        <div className='flex justify-between mt-4 mb-2'>Sign in</div>
+        <div className='flex justify-between mt-4 mb-2 pr-6'>Sign in</div>
     </div>
   )
 }
